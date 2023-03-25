f7899877b6cb31f83eb68d2c0f629f70e3621ef6
Dmitry Terehin <cheese-shop1@ya.ru>
2023-03-25 15:30:33 +0300

    The 5th sprint is solved

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+
+
 def page_not_found(request, exception):
     template = 'core/404.html'
     context = {'path': request.path}
     return render(request, template, context, status=404)
+
+
 def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
